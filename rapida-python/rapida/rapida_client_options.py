0baf884af8b83679beb4389c34d414917f16e4c3
Prashant Shrivastav <p_srivastav@outlook.com>
2024-01-18 01:49:22 +0800

    chore: fixed all the warning with variable name

--- rapida-python/rapida/rapida_client_options.py
+++ rapida-python/rapida/rapida_client_options.py
@@ -1,5 +1,53 @@
 from typing import Optional, Union
 import os
+from enum import Enum
+import warnings
+
+
+class RapidaEnvironment(Enum):
+    """
+    Rapida Environment
+    """
+    PRODUCTION = "production"
+    DEVELOPMENT = "development"
+
+    def get(self) -> str:
+        return str(self.value)
+
+    @staticmethod
+    def from_str(label):
+        if label == "production":
+            return RapidaEnvironment.PRODUCTION
+        elif label == "development":
+            return RapidaEnvironment.DEVELOPMENT
+        else:
+            warnings.warn("the environment is not supported only use production and development.")
+            return RapidaEnvironment.DEVELOPMENT
+
+
+class RapidaRegion(Enum):
+    """
+    Region supported by rapida service
+    """
+    AP = "ap"
+    US = "us"
+    EU = "eu"
+    ALL = "all"
+
+    def get(self) -> str:
+        return str(self.value)
+
+    @staticmethod
+    def from_str(label):
+        if label == "ap":
+            return RapidaRegion.AP
+        elif label == "us":
+            return RapidaRegion.US
+        elif label == "eu":
+            return RapidaRegion.EU
+        else:
+            warnings.warn("the region is not support, supported region ap, us, eu and all")
+            return RapidaRegion.ALL
 
 
 class RapidaClientOptions:
@@ -9,30 +57,29 @@ class RapidaClientOptions:
 
     rapida_api_key: str
     rapida_endpoint_url: str
-    rapida_region: Union["ap", "us", "eu", "any"]
-    rapida_environment: Optional[str]
+    rapida_region: Optional[RapidaRegion]
+    rapida_environment: Optional[RapidaEnvironment]
 
     def __init__(
-        self,
-        rapida_api_key: Optional[str] = None,
-        rapida_endpoint_url: Optional[str] = None,
-        rapida_environment: Optional[str] = "production",
-        rapida_region: Optional[Union["ap", "us", "eu"]] = "all",
+            self,
+            api_key: Optional[str] = None,
+            endpoint_url: Optional[str] = None,
+            environment: Optional[RapidaEnvironment] = RapidaEnvironment.PRODUCTION,
+            region: Optional[RapidaRegion] = RapidaRegion.ALL,
     ):
-
         """
 
         Args:
-            rapida_api_key:
-            rapida_endpoint_url:
-            rapida_region:
-            rapida_environment:
+            api_key:
+            endpoint_url:
+            region:
+            environment:
         """
-        self.rapida_api_key = rapida_api_key or os.environ.get("RAPIDA_API_KEY")
-        self.rapida_endpoint_url = rapida_endpoint_url or os.environ.get(
+        self.rapida_api_key = api_key or os.environ.get("RAPIDA_API_KEY")
+        self.rapida_endpoint_url = endpoint_url or os.environ.get(
             "RAPIDA_ENDPOINT_URL"
         )
-        self.rapida_environment = rapida_environment or os.environ.get(
+        self.rapida_environment = environment or os.environ.get(
             "RAPIDA_ENVIRONMENT"
         )
-        self.rapida_region = rapida_region or os.environ.get("RAPIDA_REGION")
+        self.rapida_region = region or os.environ.get("RAPIDA_REGION")
